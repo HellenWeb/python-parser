6558b4f36d9462d95ad39ef58beabe40367edcf4
Hellen <hellen@google.com>
2021-08-02 17:55:09 +1000

    Added a file and made changes to it

---
+++ parser.py
@@ -0,0 +1,21 @@
+
+# Modules
+
+import requests
+from bs4 import BeautifulSoup as BS
+# Logics
+
+page = 1
+
+while True:
+    r = requests.get('https://stopgame.ru/review/new/izumitelno/p' + str(page))
+    html = BS(r.content, 'html.parser')
+    items = html.select('.items > .article-summary')
+
+    if(len(items)):
+            for el in items:
+                title = el.select('.caption > a')
+                print( title[0].text )
+            page += 1    
+    else:
+        break
